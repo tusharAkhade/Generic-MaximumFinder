2f749207dda1f7e0582d73bbf6d1463988bd656c
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-19 19:10:29 +0530

    Added Test cases to find maximum of 3 integers

--- TU_MaxFinder.java
+++ TU_MaxFinder.java
@@ -4,7 +4,21 @@ import org.junit.jupiter.api.Test;
 
 public class TU_MaxFinder {
     @Test
-    void whenIntegerShouldReturnMax() {
+    void whenIntegerShouldReturnMax1() {
+        Integer int1 = 1, int2 = 6, int3 = 4;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+        Assertions.assertEquals(6, max, "Validating max method");
+    }
+
+    @Test
+    void whenIntegerShouldReturnMax2() {
+        Integer int1 = 1, int2 = 6, int3 = 4;
+        Integer max = MaxFinder.findMax(int1, int2, int3);
+        Assertions.assertEquals(6, max, "Validating max method");
+    }
+
+    @Test
+    void whenIntegerShouldReturnMax3() {
         Integer int1 = 1, int2 = 6, int3 = 4;
         Integer max = MaxFinder.findMax(int1, int2, int3);
         Assertions.assertEquals(6, max, "Validating max method");
