76a2b9cc8f8f05545a44cafb9dd07743ebe6bd91
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-20 18:54:02 +0530

    Refactor the UC1 UC2 UC3 using generic type Class

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,9 +1,18 @@
 package com.tushar.max;
 
-public class MaxFinder {
+public class MaxFinder<E> {
+    E value1;
+    E value2;
+    E value3;
 
-    public static <T extends Comparable> T findMax(T value1, T value2, T value3) {
-        T max = value1;
+    public MaxFinder(E val1, E val2, E val3) {
+        this.value1 = val1;
+        this.value2 = val2;
+        this.value3 = val3;
+    }
+
+    public <E extends Comparable> E findMax(E value1, E value2, E value3) {
+        E max = value1;
         if (value2.compareTo(max) > 0) {
             max = value2;
         }
@@ -13,5 +22,5 @@ public class MaxFinder {
         System.out.printf("Maximum of %s %s %s is %s \n", value1, value2, value3, max);
         return max;
     }
-    
+
 }
--- TU_MaxFinder.java
+++ TU_MaxFinder.java
@@ -4,65 +4,42 @@ import org.junit.jupiter.api.Test;
 
 public class TU_MaxFinder {
     @Test
-    void whenIntegerShouldReturnMax1() {
+    void whenShouldReturnMax1() {
         Integer int1 = 6, int2 = 1, int3 = 4;
-        Integer max = MaxFinder.findMax(int1, int2, int3);
-        Assertions.assertEquals(6, max, "Validating max method");
+        Float float1 = 6.1f, float2 = 1.1f, float3 = 4.1f;
+        String str1 = "Peach", str2 = "Banana", str3 = "Apple";
+        Object integerMax = new MaxFinder(int1, int2, int3).findMax(int1, int2, int3);
+        Object floatMax = new MaxFinder(float1, float2, float3).findMax(float1, float2, float3);
+        Object stringMax = new MaxFinder(str1, str2, str3).findMax(str1, str2, str3);
+        Assertions.assertEquals(6, integerMax, "Validating max method");
+        Assertions.assertEquals(6.1f, floatMax, "Validating max method");
+        Assertions.assertEquals("Peach", stringMax, "Validating max method");
     }
 
     @Test
-    void whenIntegerShouldReturnMax2() {
+    void whenShouldReturnMax2() {
         Integer int1 = 1, int2 = 6, int3 = 4;
-        Integer max = MaxFinder.findMax(int1, int2, int3);
-        Assertions.assertEquals(6, max, "Validating max method");
+        Float float1 = 1.1f, float2 = 6.1f, float3 = 4.1f;
+        String str1 = "Banana", str2 = "Peach", str3 = "Apple";
+        Object integerMax = new MaxFinder(int1, int2, int3).findMax(int1, int2, int3);
+        Object floatMax = new MaxFinder(float1, float2, float3).findMax(float1, float2, float3);
+        Object stringMax = new MaxFinder(str1, str2, str3).findMax(str1, str2, str3);
+        Assertions.assertEquals(6, integerMax, "Validating max method");
+        Assertions.assertEquals(6.1f, floatMax, "Validating max method");
+        Assertions.assertEquals("Peach", stringMax, "Validating max method");
     }
 
     @Test
-    void whenIntegerShouldReturnMax3() {
-        Integer int1 = 1, int2 = 4, int3 = 6;
-        Integer max = MaxFinder.findMax(int1, int2, int3);
-        Assertions.assertEquals(6, max, "Validating max method");
+    void whenShouldReturnMax3() {
+        Integer int1 = 4, int2 = 1, int3 = 6;
+        Float float1 = 4.1f, float2 = 1.1f, float3 = 6.1f;
+        String str1 = "Apple", str2 = "Banana", str3 = "Peach";
+        Object integerMax = new MaxFinder(int1, int2, int3).findMax(int1, int2, int3);
+        Object floatMax = new MaxFinder(float1, float2, float3).findMax(float1, float2, float3);
+        Object stringMax = new MaxFinder(str1, str2, str3).findMax(str1, str2, str3);
+        Assertions.assertEquals(6, integerMax, "Validating max method");
+        Assertions.assertEquals(6.1f, floatMax, "Validating max method");
+        Assertions.assertEquals("Peach", stringMax, "Validating max method");
     }
 
-    @Test
-    void whenFloatShouldReturnMax1() {
-        Float num1 = 11.2f, num2 = 6.2f, num3 = 4.3f;
-        Float max = MaxFinder.findMax(num1, num2, num3);
-        Assertions.assertEquals(11.2f, max, "Validating max method");
-    }
-
-    @Test
-    void whenFloatShouldReturnMax2() {
-        Float num1 = 4.3f, num2 = 11.2f, num3 = 6.2f;
-        Float max = MaxFinder.findMax(num1, num2, num3);
-        Assertions.assertEquals(11.2f, max, "Validating max method");
-    }
-
-    @Test
-    void whenFloatShouldReturnMax3() {
-        Float num1 = 4.3f, num2 = 6.2f, num3 = 11.2f;
-        Float max = MaxFinder.findMax(num1, num2, num3);
-        Assertions.assertEquals(11.2f, max, "Validating max method");
-    }
-
-    @Test
-    void whenStringShouldReturnMax1() {
-        String str1="Peach", str2 = "Apple", str3 = "Banana";
-        String maxString = MaxFinder.findMax(str1, str2, str3);
-        Assertions.assertEquals("Peach", maxString, "Validating maxFinder function");
-    }
-
-    @Test
-    void whenStringShouldReturnMax2() {
-        String str1="Apple", str2 = "Peach", str3 = "Banana";
-        String maxString = MaxFinder.findMax(str1, str2, str3);
-        Assertions.assertEquals("Peach", maxString, "Validating maxFinder function");
-    }
-
-    @Test
-    void whenStringShouldReturnMax3() {
-        String str1="Banana", str2 = "Apple", str3 = "Peach";
-        String maxString = MaxFinder.findMax(str1, str2, str3);
-        Assertions.assertEquals("Peach", maxString, "Validating maxFinder function");
-    }
 }
