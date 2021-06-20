8ffb6d4b5dc2719e26c0c085c75f510af4060004
tusharAkhade <84910751+tusharAkhade@users.noreply.github.com>
2021-06-20 12:33:27 +0530

    Added test cases to max finder

--- TU_MaxFinder.java
+++ TU_MaxFinder.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class TU_MaxFinder {
     @Test
     void whenIntegerShouldReturnMax1() {
-        Integer int1 = 1, int2 = 6, int3 = 4;
+        Integer int1 = 6, int2 = 1, int3 = 4;
         Integer max = MaxFinder.findMax(int1, int2, int3);
         Assertions.assertEquals(6, max, "Validating max method");
     }
@@ -19,7 +19,7 @@ public class TU_MaxFinder {
 
     @Test
     void whenIntegerShouldReturnMax3() {
-        Integer int1 = 1, int2 = 6, int3 = 4;
+        Integer int1 = 1, int2 = 4, int3 = 6;
         Integer max = MaxFinder.findMax(int1, int2, int3);
         Assertions.assertEquals(6, max, "Validating max method");
     }
