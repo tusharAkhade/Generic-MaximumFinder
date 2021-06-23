58bf3ebb5281c3ab27faf8b6bdc084b454ec713f
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-24 02:17:35 +0530

    Refactor to find max of more than 3 parameters

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,19 +1,101 @@
 package com.tushar.max;
+import java.util.Scanner;
+
 /**
-* MaxFinder will find the maximum of three integers
-* @author Tushar Akhade
-* @since 20 Jun 2021
-*/
+ * Finding the maximum value in Integer, Float, String array without Generic type.
+ * @author Tushar Akhade
+ * @since 20 Jun 2021
+ */
+
 public class MaxFinder {
-    public static Integer findMax(Integer int1, Integer int2, Integer int3) {
-        Integer max = int1;
-        if (int2.compareTo(max) > 0) {
-            max = int2;
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Press 1 to find maximum of Integers\nPress 2 to find maximum of Float number\nPress 3 to find maximum of String");
+        System.out.print("Enter option : ");
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.println("Enter a size of Integer array: ");
+                int size = scanner.nextInt();
+                Integer[] integersArray = new Integer[size];
+                integersArray = printInteger(integersArray, size);
+                Integer integerMax = findIntegerMax(integersArray);
+                System.out.println("Maximum integer value is :" + integerMax);
+                break;
+            case 2:
+                System.out.println("Enter a size of float array: ");
+                size = scanner.nextInt();
+                Float[] floatsArray = new Float[size];
+                floatsArray = printFloat(floatsArray, size);
+                Float floatMax = findFloatMax(floatsArray);
+                System.out.println("Maximum float number is :" + floatMax);
+                break;
+            case 3:
+                System.out.println("Enter a size of String array: ");
+                size = scanner.nextInt();
+                String[] stringsArray = new String[size];
+                stringsArray = printString(stringsArray, size);
+                String stringMax = findStringMax(stringsArray);
+                System.out.println("Maximum String is :" + stringMax);
+                break;
+            default:
+                System.out.println("Invalid input.");
         }
-        if (int3.compareTo(max) > 0) {
-            max = int3;
+    }
+
+    public static Integer[] printInteger(Integer[] integersArray, int size) {
+        for (int i = 0; i<size; i++) {
+            System.out.println("Enter array value : ");
+            integersArray[i] = scanner.nextInt();
         }
-        System.out.printf("Max values of %s %s %s is %s \n", int1, int2, int3, max);
+        return integersArray;
+    }
+
+    public static Float[] printFloat(Float[] floatsArray, int size) {
+        for (int i = 0; i<size; i++) {
+            System.out.println("Enter array value : ");
+            floatsArray[i] = scanner.nextFloat();
+        }
+        return floatsArray;
+    }
+
+    public static String[] printString(String[] stringsArray, int size) {
+        for (int i = 0; i<size; i++) {
+            System.out.println("Enter array value : ");
+            stringsArray[i] = scanner.next();
+        }
+        return stringsArray;
+    }
+
+
+    public static Integer findIntegerMax(Integer[] integers) {
+        Integer max = integers[0];
+        for (Integer i : integers) {
+            if (i.compareTo(max) > 0) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    public static Float findFloatMax(Float[] floats) {
+        Float max = floats[0];
+        for (Float i : floats) {
+            if (i.compareTo(max) > 0) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    public static String findStringMax(String[] strings) {
+        String max = strings[0];
+        for (String i : strings) {
+            if (i.compareTo(max) > 0) {
+                max = i;
+            }
+        }
         return max;
     }
 }
